480a391e0512bf2e895ad58b07be1d7cc29d5be0
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-26 21:04:00 +0530

    connected successfully mongo db

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,15 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello Varaliya!')
 })
+
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
