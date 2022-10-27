9fb57ec423c271487532d0443ff7be78da2e44e2
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-27 21:40:06 +0530

    created user schema

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,30 +11,32 @@ router.post('/createuser', [
 ], async (req, res) => {
 
     // If there are errors, return bad request and the errors
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    // Check whether the user with the email exists already
-    let user = await User.findOne({ email: req.body.email });
-    
-    if (user){
-        return res.status(400).json({error: 'Sorry a user with this email already exists'})
-    }
+    try {
 
-    user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        date: req.body.date
-    })
-    
-    // .then(user => res.json(user))
-    // .catch(err => { console.log(err), res.json
-    //     ({ error: 'Please enter a unique value for email', message: err.message }) });
-    res.json(user);
+        // Check whether the user with the email exists already
+        let user = await User.findOne({ email: req.body.email });
+
+        if (user) {
+            return res.status(400).json({ error: 'Sorry a user with this email already exists' })
+        }
+
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            date: req.body.date
+        })
+
+        res.json(user);
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("some error occured");
+    }
 })
 
 module.exports = router
