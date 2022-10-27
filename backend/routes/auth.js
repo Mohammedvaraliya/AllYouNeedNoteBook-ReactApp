7a08dbad34c378bb4c50dcf428462abacd1f45d1
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-27 23:15:26 +0530

    created JWT token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 // Creating a User using : POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -25,11 +26,14 @@ router.post('/createuser', [
             return res.status(400).json({ error: 'Sorry a user with this email already exists' })
         }
 
+        const salt = bcrypt.genSalt(10, function(err, salt) {});
+        const secPass = bcrypt.hashSync(req.body.password, salt);
+
         // Create a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
             date: req.body.date
         })
 
