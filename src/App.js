d297774157da8c2c31ebfd8b57e1fcada642ffb8
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-10 21:41:48 +0530

    created app with routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my react app</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
