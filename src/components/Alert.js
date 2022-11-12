3506a857e7a64d2f2ae522116c58f40aeeba9309
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-12 13:11:28 +0530

    created Alert from bootstrap

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ const Alert = (props) => {
     return (
         <>
         <div>
-            <div className="alert alert-warning alert-dismissible fade show" role="alert">
+            <div className="alert alert-primary alert-dismissible fade show" role="alert">
                 <strong>Holy guacamole!</strong> {props.message}.
                 <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
