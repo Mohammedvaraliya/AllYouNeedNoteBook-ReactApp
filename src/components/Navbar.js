91c66acf6f70afa4f15ab54ee065d79fc147d48d
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-11-20 22:03:53 +0530

    Fix bug on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = (props) => {
 
     let location = useLocation();
     useEffect(() => {
-        userDetail()
+        userDetail();
     }, [location]);
 
     const handleLogout = () => {
